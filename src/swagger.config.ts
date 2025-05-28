729b7aa719b439798429db5d39c76e4f46c0bcdb
JossAbreu <josue.abreu.0712@hotmail.com>
2025-05-28 11:36:49 -0400

    docs: :memo: Add note in documentation

--- src/swagger.config.ts
+++ src/swagger.config.ts
@@ -5,7 +5,11 @@ export const config = new DocumentBuilder()
   .setTitle('BANFONDESA API 📚',)
 
   .setDescription(`Desarrollar e implementar una API REST utilizando NestJS, asegurando autenticación con JWT,
- documentación con Swagger y almacenamiento en una base de datos PostgreSQL. BY: Josue abreu de la rosa`)
+ documentación con Swagger y almacenamiento en una base de datos PostgreSQL. BY: Josue abreu de la rosa 
+ 
+ 
+  NOTA:Para probar los enpoints de la API, primero debes autenticarte y obtener un token JWT.
+  Luego, puedes usar ese token en los headers de tus solicitudes a los endpoints protegidos.`)
   .setContact('Banfondesa', 'https://banfondesa.com.do', 'info@banfondesa.com.do')
   .setLicense('MIT', 'https://opensource.org/licenses/MIT')
   .setVersion('1.0')
@@ -17,7 +21,7 @@ export const config = new DocumentBuilder()
       name: 'Authorization',
       in: 'header',
     },
-    'access-token', // Este es el nombre del esquema (usado para referencias)
+    'access-token',
 
   )
   .build();
