03bb7815aff2cdf7d5507aff402eae1ed69ad881
JossAbreu <josue.abreu.0712@hotmail.com>
2025-05-28 13:54:14 -0400

    Update swagger.config.ts

--- src/swagger.config.ts
+++ src/swagger.config.ts
@@ -11,7 +11,6 @@ export const config = new DocumentBuilder()
   NOTA:Para probar los enpoints de la API, primero debes autenticarte y obtener un token JWT.
   Luego, puedes usar ese token en los headers de tus solicitudes a los endpoints protegidos.`)
   .setContact('Banfondesa', 'https://banfondesa.com.do', 'info@banfondesa.com.do')
-  .setLicense('MIT', 'https://opensource.org/licenses/MIT')
   .setVersion('1.0')
   .addServer('http://localhost:3000', 'Development Server')
   .addServer('https://banfondesa-api-production.up.railway.app', 'Production Server')
